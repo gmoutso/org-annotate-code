d2a97eca6741a067bf695b5d0baf653b41e157fd
George Moutsopoulos <george.moutsopoulos@ev.uk>
2020-07-11 21:46:40 +0100

    corrections to search

--- org-annotate-word.el
+++ org-annotate-word.el
@@ -14,24 +14,152 @@
 ;;; Code:
 (require 'org-annotate-code)
 (require 'ol)
+
+;; links
+
 (org-link-set-parameters "word"
                          :follow 'org-annotate-word-search
-                         :export #'orgorg-annotate-word-export
-                         :store #'org-annotate-word-store-link)
+			 :followstrict 'org-annotate-word-search-strict
+			 :followchoose 'org-annotate-word-search-choose
+                         :export 'orgorg-annotate-word-export
+                         :store 'org-annotate-word-store-link)
 
-(defun org-annotate-word-get-word  ()
+(defun org-annotate-word-store-link ()
+  "Store a link."
+  (let* ((filename (expand-file-name (buffer-file-name)))
+	 (word (org-annotate-word-get-word-at-point))
+	 (lineno (line-number-at-pos))
+	 (description nil))
+      (if word (org-link-store-props
+		:type "word"
+		:link (format "%s::%s::%s" filename lineno word)))))
+
+(defun org-annotate-word-export (link description format)
+  "Export LINK with DESCRIPTION into FORMAT."
+  (let ((path link)
+        (desc (or description link)))
+    (pcase format
+      ;; (`html (format "%s" path))
+      ;; (`latex (format "\\href{%s}{%s}" path desc))
+      ;; (`texinfo (format "@uref{%s,%s}" path desc))
+      ;; (`ascii (format "%s (%s)" desc path))
+      (_ (if description (format "%s (%s)" desc path) path)))))
+
+;; parsing
+
+(defun org-annotate-word-get-word-at-point  ()
   "Return word at point."
-  (let* ((word (word-at-point)))
-    (unless word
-      (cond ((word-search-forward  ".*" nil t) (setq word (word-at-point)))
-	    ((word-search-backward ".*" nil t) (setq word (word-at-point)))
-	    (t (setq word ""))))
-    (substring-no-properties word)))
+  (when (word-at-point)
+    (substring-no-properties (word-at-point))))
+
+;; (defun org-annotate-word-get-word-nearest  ()
+;;   "Return word nearest to point."
+;;   (substring-no-properties
+;;    (cond ((word-at-point))
+;; 	 ((word-search-forward  ".*" nil t))
+;; 	 ((word-search-backward ".*" nil t)))))
+
+;; follow
+
+(defun org-annotate-word-move-within-word ()
+  (beginning-of-thing 'word))
+
+(defun org-annotate-word-goto-word-choose (lineno word)
+  "Choose WORD at LINENO.
+Return nil if not found."
+  (org-annotate-word-goto-word lineno word)
+  (let ((list-matching-lines-jump-to-current-line t))
+      (occur word)
+      (org-annotate-word-goto-word-strict (line-number-at-pos) word))
+  (if (word-at-point) (org-annotate-word-move-within-word)))
+
+(defun org-annotate-word-goto-word-strict (lineno word)
+  "Goto first WORD at LINENO.
+Return nil if not found."
+  (let ((found
+	 (save-excursion 
+	   (goto-char (point-min))
+	   (forward-line lineno)
+	   (search-forward word (point-at-eol) t))))
+    (when found
+      (goto-char found)
+      (beginning-of-thing 'word)
+      (point))))
+
+(defun org-annotate-word-goto-word (lineno word &optional window)
+  "Goto WORD near LINENO.
+Return nil if not found."
+  (let ((p (point))
+	(found (org-annotate-word-goto-word-strict lineno word))
+	find1 find2)
+    (unless found
+      (setq find1 (save-excursion
+		     (goto-char (point-min))
+		     (forward-line lineno)
+		     (when (search-forward word
+					 (when window (point-at-eol (+ 1 window)))
+					 t)
+			 (point))))
+      (setq find2 (save-excursion
+		     (goto-char (point-min))
+		     (forward-line lineno)
+		     (when (search-backward word
+					  (when window (point-at-bol (- 1 window)))
+					  t)
+			 (point))))
+      (setq found (cond ((and (not find2) (not find2))
+			   nil) ;; return nil
+			  ((and (not find1) find2)
+			   find2)
+			  ((and (not find2) find1)
+			   find1)
+			  ((> (- lineno (line-number-at-pos find2)) (- (line-number-at-pos find1) lineno))
+			   find1)
+			  ((< (- lineno (line-number-at-pos find2)) (- (line-number-at-pos find1) lineno))
+			   find2))))
+    (when found
+      (goto-char found)
+      (beginning-of-thing 'word))
+    ))
+
+;; folowing link
 
 (defun org-annotate-word-split-filename-lineno-searchname (link)
   "Return list of file::no::word from LINK."
   (string-match "\\(?1:[-[:alnum:]_\\./]*\\)::\\(?2:[0-9]*\\)::\\(?3:[-[:alnum:]_\\./]*\\)" link)
   (list (match-string-no-properties 1 link) (string-to-number (match-string-no-properties 2 link)) (match-string-no-properties 3 link)))
+
+(defun org-annotate-word-get-lineno (link)
+  "Return line number of LINK."
+  (nth 1 (org-annotate-word-split-filename-lineno-searchname link)))
+
+(defun org-annotate-word-search (link &optional window)
+  "Search for link.
+Return nil if not found."
+  (let* ((splitlink (org-annotate-word-split-filename-lineno-searchname link))
+	 (filename (nth 0 splitlink))
+	 (lineno (nth 1 splitlink))
+	 (word (nth 2 splitlink)))
+    (find-file filename)
+    (org-annotate-word-goto-word lineno word window)))
+
+(defun org-annotate-word-search-strict (link)
+  (let* ((splitlink (org-annotate-word-split-filename-lineno-searchname link))
+	 (filename (nth 0 splitlink))
+	 (lineno (nth 1 splitlink))
+	 (word (nth 2 splitlink)))
+    (find-file filename)
+    (org-annotate-word-goto-word-strict lineno word)))
+
+(defun org-annotate-word-search-choose (link)
+  (let* ((splitlink (org-annotate-word-split-filename-lineno-searchname link))
+	 (filename (nth 0 splitlink))
+	 (lineno (nth 1 splitlink))
+	 (word (nth 2 splitlink)))
+    (find-file filename)
+    (org-annotate-word-goto-word-choose lineno word)))
+
+;; annotation
 
 (defun org-annotate-word-add-filename-node (filename annotation)
   "Add the FILENAME as top-level node to ANNOTATION."
@@ -50,62 +178,13 @@
 				:heading (format "%s near %s" word lineno)))))
 	(org-annotate-word-add-filename-node filename annotation)))
 
-(defun org-annotate-word-store-link ()
-  "Store a link."
-  (let* ((filename (expand-file-name (buffer-file-name)))
-	 (word (org-annotate-word-get-word))
-	 (lineno (line-number-at-pos))
-	 (description nil))
-      (org-link-store-props
-       :type "word"
-       :link (format "%s::%s::%s" filename lineno word)
-       :description description)))
-
-(defun org-annotate-word-export (link description format)
-  "Export LINK with DESCRIPTION into FORMAT."
-  (let ((path link)
-        (desc (or description link)))
-    (pcase format
-      ;; (`html (format "%s" path))
-      ;; (`latex (format "\\href{%s}{%s}" path desc))
-      ;; (`texinfo (format "@uref{%s,%s}" path desc))
-      ;; (`ascii (format "%s (%s)" desc path))
-      (_ (if description (format "%s (%s)" desc path) path)))))
-
-(defun org-annotate-word-goto-word (lineno word)
-  "Goto WORD near LINENO."
-  (let (find1 find2)
-    (goto-line lineno)
-    (search-forward word nil t)
-    (setq find1 (point))
-    (goto-line lineno)
-    (search-backward word nil t)
-    (setq find2 (point))
-    (cond ((and (not find2) (not find2))
-	   (message (format "word %s not found" word))
-	   (goto-char (point-min)))
-	  ((and (not find1) find2)
-	   (goto-char find2))
-	  ((and (not find2) find1)
-	   (goto-char find1))
-	  ((> (- (line-number-at-pos find2) lineno) (- (line-number-at-pos find1) lineno))
-	   (goto-char find1))
-	  (t (goto-char find2)))))
-
-(defun org-annotate-word-search (link)
-  (let* ((splitlink (org-annotate-word-split-filename-lineno-searchname link))
-	 (filename (nth 0 splitlink))
-	 (lineno (nth 1 splitlink))
-	 (word (nth 2 splitlink)))
-    (if filename (find-file filename))
-    (org-annotate-word-goto-word lineno word)))
-
 (defun org-annotate-word-info-at-point ()
   "Return a plist with word info at point."
   (let* ((filename (buffer-file-name))
 	 (lineno (line-number-at-pos))
-	 (word (org-annotate-word-get-word)))
-	 (org-annotate-word-make-annotation-from-word filename lineno word)))
+	 (word (org-annotate-word-get-word-at-point)))
+    (if word
+	 (org-annotate-word-make-annotation-from-word filename lineno word))))
 
 (provide 'org-annotate-word)
 ;;; org-annotate-word.el ends here
